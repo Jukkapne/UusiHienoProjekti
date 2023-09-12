755fa40161eab8ee5bf646f6ad6a9718f1269a2e
Jukkapne <jukka.nevalainen@oamk.fi>
2023-09-12 16:19:19 +0300

    examples

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,18 @@
-document.getElementById('btn').onclick = function() {
+
+document.getElementById('btn2').onclick = function() {
     this.style.backgroundColor = 'red';
   };
+
+
   document.getElementById('toggleClassButton').onclick = function() {
     this.classList.toggle('active');
   };
-  document.getElementById('hoverButton').onmouseover = function() {
-    this.style.backgroundColor = 'orange';
-  };
+
+
+
+ 
   
-  document.getElementById('hoverButton').onmouseout = function() {
-    this.style.backgroundColor = 'yellow';
-  };
+
   document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Form submitted!');
