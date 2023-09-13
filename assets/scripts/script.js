01e3ea51c170a0df5c22076ab14158737207c4a1
Jukkapne <jukka.nevalainen@oamk.fi>
2023-09-13 10:20:22 +0300

    api example

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,24 @@
-
-document.getElementById('btn2').onclick = function() {
-    this.style.backgroundColor = 'red';
-  };
-
-
-  document.getElementById('toggleClassButton').onclick = function() {
-    this.classList.toggle('active');
-  };
-
-
-
- 
-  
-
-  document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted!');
-  });
-    
+async function fetchData() {
+  try {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    let data = await response.json();
+    let resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `
+      <strong>Title:</strong> ${data.title}<br>
+      <strong>Body:</strong> ${data.body}
+    `;
+    resultDiv.style.backgroundColor = '#e0ffe0';
+    resultDiv.style.border = '2px solid #00cc00';
+    resultDiv.style.borderRadius = '15px';
+  } catch (error) {
+    console.error('Fetch error:', error);
+    let resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = 'Error fetching data';
+    resultDiv.style.backgroundColor = '#ffe0e0';
+    resultDiv.style.border = '2px solid #cc0000';
+    resultDiv.style.borderRadius = '15px';
+  }
+}
