03497b4ff80d229f2cc2e3cd8760a134e63459e2
Jukkapne <jukka.nevalainen@oamk.fi>
2023-09-25 10:58:40 +0300

    ajastuksella tyylin vaihto sivulle

---
+++ assets/scripts/ajastusscritps.js
@@ -0,0 +1,10 @@
+    // Check the current date and apply themes accordingly
+    const currentDate = new Date();
+
+    if (currentDate.getMonth() === 9 && currentDate.getDate() === 31) {
+        // Halloween - October 31
+        document.body.classList.add('halloween');
+    } else if (currentDate.getMonth() === 11 && currentDate.getDate() >= 24 && currentDate.getDate() <= 26) {
+        // Christmas - December 24-26
+        document.body.classList.add('christmas');
+    }
